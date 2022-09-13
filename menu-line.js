5a1e1098574b58d6a53f2609c14f540996198961
Utkarsh Gupta <69791796+Utkarsh2902@users.noreply.github.com>
2022-09-13 21:55:42 +0530

    Add files via upload

---
+++ menu-line.js
@@ -0,0 +1,28 @@
+$(function() {
+    var $el, leftPos, newWidth,
+        $mainNav = $("#menu-links");
+    
+    $mainNav.append("<li id='magic-line'></li>");
+    var $magicLine = $("#magic-line");
+    
+    $magicLine
+        .width($(".current_page_item").width())
+        .css("left", $(".current_page_item a").position().left)
+        .data("origLeft", $magicLine.position().left)
+        .data("origWidth", $magicLine.width());
+        
+    $("#menu-links li a").hover(function() {
+        $el = $(this);
+        leftPos = $el.position().left;
+        newWidth = $el.parent().width();
+        $magicLine.stop().animate({
+            left: leftPos,
+            width: newWidth
+        });
+    }, function() {
+        $magicLine.stop().animate({
+            left: $magicLine.data("origLeft"),
+            width: $magicLine.data("origWidth")
+        });    
+    });
+});
